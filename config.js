a5ebcc0caf6e08c88f67734b195c2e94b5bddfb0
Bhojappa <bhojappapawar18@gmail.com>
2023-08-27 23:42:28 +0530

    changes

--- config.js
+++ config.js
@@ -3,28 +3,45 @@ var mysql = require("mysql");
 var pool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "9380537486",
-    database: "dbms_project",
+    password: "Virat@18",
+    database: "travel_services",
     multipleStatements: true
 });
 
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Virat@18",
+    database: "travel_services",
+    multipleStatements: true
+})
+
+con.connect(function (err) {
+    if (err) {
+        console.log("HELLO")
+        console.log(err)
+    } else {
+        console.log("database Connected")
+    }
+})
+
 module.exports = {
-    executeQuery : function(query, callback) {
-        pool.getConnection(function(err, connection) {
-            if(err) {
+    executeQuery: function (query, callback) {
+        pool.getConnection(function (err, connection) {
+            if (err) {
                 connection.release;
                 console.log("Error connecting" + err);
                 return;
             } else {
-                connection.query(query, function(err, rows) {
+                connection.query(query, function (err, rows) {
                     connection.release();
-                    if(!!err) console.log("Error executing " + err);
+                    if (!!err) console.log("Error executing " + err);
                     else {
                         console.log("Query Executed");
                         callback(null, rows);
                     }
                 });
-                connection.on("error", function(err) {
+                connection.on("error", function (err) {
                     console.log("Error on " + err);
                 });
             }
